44d7c037ad489f53162c4c83ca61973e300d4ec0
GastonQuelali <gaston_q@hotmail.com>
2025-05-02 13:29:41 -0400

    Add Classroom API

--- ejemplos/e3-csharp/Migrations/20250502172807_AddClassroom.Designer.cs
+++ ejemplos/e3-csharp/Migrations/20250502172807_AddClassroom.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using e3_csharp.Data;
 
@@ -10,9 +11,11 @@ using e3_csharp.Data;
 namespace e3_csharp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250502172807_AddClassroom")]
+    partial class AddClassroom
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
