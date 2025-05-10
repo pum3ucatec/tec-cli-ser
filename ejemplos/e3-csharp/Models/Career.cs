79ac68e64c48265c78ca0017bb188247e427467e
EsteysiLindi <gesteysi0@gmail.com>
2025-05-10 11:15:45 -0400

    segundainstanciafinalisada

---
+++ ejemplos/e3-csharp/Models/Career.cs
@@ -0,0 +1,8 @@
+namespace e3_csharp.Models;
+public class Career
+{
+    public int ID { get; set; }
+    public string Name { get; set; } = string.Empty;   // Inicializado
+    public string Description { get; set; } = string.Empty;    // Inicializado
+    
+}
