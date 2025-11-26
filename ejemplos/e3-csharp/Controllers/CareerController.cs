b6fe759763ead972cfb188cd9b8f837931fcdcdc
GastonQuelali <gaston_q@hotmail.com>
2025-11-26 19:12:04 -0400

    Career

---
+++ ejemplos/e3-csharp/Controllers/CareerController.cs
@@ -0,0 +1,157 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using e3_csharp.Data;
+using e3_csharp.Models;
+
+namespace e3_csharp.Controllers
+{
+    public class CareerController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CareerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Career
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Careers.ToListAsync());
+        }
+
+        // GET: Career/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var career = await _context.Careers
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (career == null)
+            {
+                return NotFound();
+            }
+
+            return View(career);
+        }
+
+        // GET: Career/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Career/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,Name,Description,Status")] Career career)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(career);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(career);
+        }
+
+        // GET: Career/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var career = await _context.Careers.FindAsync(id);
+            if (career == null)
+            {
+                return NotFound();
+            }
+            return View(career);
+        }
+
+        // POST: Career/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,Name,Description,Status")] Career career)
+        {
+            if (id != career.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(career);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CareerExists(career.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(career);
+        }
+
+        // GET: Career/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var career = await _context.Careers
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (career == null)
+            {
+                return NotFound();
+            }
+
+            return View(career);
+        }
+
+        // POST: Career/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var career = await _context.Careers.FindAsync(id);
+            if (career != null)
+            {
+                _context.Careers.Remove(career);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CareerExists(int id)
+        {
+            return _context.Careers.Any(e => e.ID == id);
+        }
+    }
+}
